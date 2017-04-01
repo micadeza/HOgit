e894ca412ea5085df18773e483e543a2d2e33a81
micadeza <miica.vl@hotmail.com>
2017-04-01 14:21:47 -0300

    Poblar red cuadrada,  con función aleatoria

--- tp.c
+++ tp.c
@@ -2,29 +2,22 @@
 #include <stdlib.h>
 #include <time.h>
 
+void aleatorio (int *m1,int n,int m,float p);
+
 int main () {
 	srand(time(NULL));
 	int *m1;
 	int i;
 	int n=10; //filas
 	int m=10;  //columnas	
-	float p=0.5;
-	float a;
+	//float p=0.5;
+	
 	
 	
 	m1= (int*)malloc(n*m*sizeof(int));
 	
-	for (i=0;i<n*m;i++) {
-			a=(rand()/(float)RAND_MAX);	
-			//printf("%f\n",a);		
-			if (p<a) {
-				m1[i]=0;
-			} 
-			else {
-				m1[i]=1;
-			}
-		}
-	//printf("%d\n",m1[4]);
+	
+	aleatorio (m1, 10, 10, 0.5);
 	
 	
 	
@@ -37,4 +30,21 @@ int main () {
 	}
 	
 	return 0;
+}
+
+
+void aleatorio (int *m1,int n,int m,float p) {
+	float a;
+	int i;
+	for (i=0;i<n*m;i++) {
+			a=(rand()/(float)RAND_MAX);	
+			//printf("%f\n",a);		
+			if (p<a) {
+				m1[i]=0;
+			} 
+			else {
+				m1[i]=1;
+			}
+	}
+
 }
